d6a1346ac9370dc0286b81b52f01ee74010e0b3e
novacrazy <novacrazy@gmail.com>
2020-05-01 14:55:06 -0500

    Temporarily silence warning

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@
 //!
 //! * Most functions which take a string slice as input may possibly panic in the unlikely event that a c style string cannot be created based on it. (IE if your slice already has a null byte in it)
 
+#![allow(unused, deprecated)]
 #![deny(missing_debug_implementations)]
 
 #[macro_use]
